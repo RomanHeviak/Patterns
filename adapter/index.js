bb068b466b3276261ed33616c7774202f4058991
Roman Heviak <102031342+Roman-Heviak@users.noreply.github.com>
2023-06-30 16:06:29 +0300

    adapter

---
+++ adapter/index.js
@@ -0,0 +1,35 @@
+class RedisDataManager {
+    constructor() {
+        this.connect = function() {
+            console.log('Connect to database');
+        };
+        this.scan = function() {
+            return 'Data from database';
+        };
+    }
+}
+class DataManager {
+    constructor() {
+        this.getData = function() {
+            return 'Legacy data';
+        }
+    }
+}
+class Adapter {
+    constructor() {
+        this.redis = new RedisDataManager();
+        this.redis.connect();
+        this.getData = function() {
+            return this.redis.scan();
+        }
+    }
+    
+    
+}
+class Client {
+    constructor(dataManager) {
+        console.log(dataManager.getData());
+    }
+    
+}
+let client = new Client(new Adapter());
